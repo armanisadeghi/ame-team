f1ae0e71907325546f4372762fb5182dba15a25c
armanisadeghi <132974515+armanisadeghi@users.noreply.github.com>
2024-05-11 08:01:46 -0700

    updates

--- automation_matrix/processing/ai_output.py
+++ automation_matrix/processing/ai_output.py
@@ -763,39 +763,40 @@ async def local_post_processing(sample_content):
                         "extractors": [
                             {
                                 "name": "access_data_by_reference",
+                                "broker": "BLOG_IDEA_1",
                                 "key_identifier": "nested_structure",
                                 "key_index": 1,
                                 "output_type": "text",
-                                "broker": "BLOG_IDEA_1"
                             },
                             {
                                 "name": "access_data_by_reference",
                                 "key_identifier": "nested_structure",
                                 "key_index": 2,
                                 "output_type": "text",
-                                "broker": "BLOG_IDEA_2"
+                                "broker": "BLOG_IDEA_2",
                             },
                             {
                                 "name": "access_data_by_reference",
                                 "key_identifier": "nested_structure",
                                 "key_index": 3,
                                 "output_type": "text",
-                                "broker": "BLOG_IDEA_3"
+                                "broker": "BLOG_IDEA_3",
                             },
                             #  I have commented out Blog Idea 4 for demonstration purposes as though the request was to get blogs 1, 2, 3, and 5, but NOT 4
                             {
                                 "name": "access_data_by_reference",
+                                "broker": "BLOG_IDEA_4",
                                 "key_identifier": "nested_structure",
                                 "key_index": 4,
                                 "output_type": "text",
-                                "broker": "BLOG_IDEA_4"
+
                             },
                             {
                                 "name": "access_data_by_reference",
+                                "broker": "BLOG_IDEA_5",
                                 "key_identifier": "nested_structure",
                                 "key_index": 5,
                                 "output_type": "text",
-                                "broker": "BLOG_IDEA_5"
                             }
                         ]
                     }
